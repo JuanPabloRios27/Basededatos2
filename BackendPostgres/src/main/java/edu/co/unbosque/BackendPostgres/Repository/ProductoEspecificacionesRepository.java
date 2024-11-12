2ed96af9f7c5027da54485fa5a0a2e2feca39e72
JuanPabloRios27 <jprios@unbosque.edu.co>
2024-11-12 17:15:05 -0500

    Eliminación java

--- BackendPostgres/src/main/java/edu/co/unbosque/BackendPostgres/Repository/ProductoEspecificacionesRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.co.unbosque.BackendPostgres.Repository;
-
-import edu.co.unbosque.BackendPostgres.model.ProductoEspecificaciones;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoEspecificacionesRepository extends JpaRepository<ProductoEspecificaciones, Long> {
-}
--- BackendPostgres/src/main/java/edu/co/unbosque/BackendPostgres/Repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.co.unbosque.BackendPostgres.Repository;
-
-import edu.co.unbosque.BackendPostgres.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
