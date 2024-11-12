a13b90e18440796bc665466e45d4e664991a275c
JuanPabloRios27 <jprios@unbosque.edu.co>
2024-11-12 17:11:34 -0500

    Actualización del proyecto
    actu

--- BackendPostgres/src/main/java/edu/co/unbosque/BackendPostgres/model/Entidad.java
+++ BackendPostgres/src/main/java/edu/co/unbosque/BackendPostgres/model/Entidad.java
@@ -1,4 +1,15 @@
 package edu.co.unbosque.BackendPostgres.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "entidad")
 public class Entidad {
+	@Id
+	private long idEntidad;
+	private long NIT;
+	
+	
 }
